d617cc36523241884e34905b3d5e634d95cef617
angelo <angelo.belcastro01@gmail.com>
2023-06-09 19:35:20 +0200

    Rinominato file principale

--- src/main/java/exm/sisinf/webpdm/WebPDMApp.java
+++ src/main/java/exm/sisinf/webpdm/WebPDMApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebpdmApplication {
+public class WebPDMApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebpdmApplication.class, args);
+        SpringApplication.run(WebPDMApp.class, args);
     }
 
 }
